2bfc991712832bf1199f73021cbfab0b39e8fb58
Ahmed Nabawi <ahmed.elshihaly6060@gmail.com>
2024-11-30 10:25:11 +0200

    End of Section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,98 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController(DataContext context) : ControllerBase
+    {
+
+
+        #region  Methods
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await context.Users.ToListAsync();
+            return Ok(new { users });
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await context.Users.FindAsync(id);
+            if (user is null)
+                return NotFound($"User with id {id} not exist");
+
+            return Ok(new { user });
+        }
+
+        [HttpPost]
+        public ActionResult<AppUser> AddUser(AppUser model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest("Invalid data");
+
+                context.Users.Add(model);
+                context.SaveChanges();
+
+                var recentlyAdded = context.Users.Find(model.Id);
+                return Ok(new { recentlyAdded });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> EditUser(int id, AppUser model)
+        {
+            try
+            {
+                var user = await context.Users.FindAsync(id);
+                if (user is null)
+                    return NotFound("user not found");
+
+                user.UserName = model.UserName;
+                context.Update(user);
+                await context.SaveChangesAsync();
+
+                return Ok(new { user });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                var user = await context.Users.FindAsync(id);
+                if (user is null)
+                    return NotFound("user not found");
+
+                context.Remove(user);
+                await context.SaveChangesAsync();
+                return Ok("User deleted");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        #endregion
+
+    }
+}
