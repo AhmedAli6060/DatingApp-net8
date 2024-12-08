deec5ca89d02480399d1321f7b63dbc9e7e63ada
Ahmed Nabawi <ahmed.elshihaly6060@gmail.com>
2024-12-08 13:20:54 +0200

    End of Section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,36 +2,46 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_Services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { AppComponent } from '../app.component';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule,BsDropdownModule],
+  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive,TitleCasePipe],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
 export class NavComponent {
   accountService = inject(AccountService);
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
   model: any = {};
 
   login() {
-    this.accountService.login(this.model).subscribe({
-      next: (response) => {
-        console.log(response);
-        this.model.username=this.model.password = "";
-      },
-      error: (err) => {
-        console.log(err);
-        alert('Some thing went wrong! \n'+err.error);
-      },
-    });
+    console.log(this.model);
+    if (!this.model.username) {
+      this.toastr.warning('Please enter username');
+    } else if (!this.model.password) {
+      this.toastr.warning('Please enter password');
+    } else {
+      this.accountService.login(this.model).subscribe({
+        next: (_) => {
+          this.router.navigateByUrl('/members');
+          this.model.username = this.model.password = '';
+        },
+        error: (err) => {
+          console.log(err);
+          this.toastr.error(err.error);
+        }
+      });
+    }
   }
 
-
-  logout(){
+  logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
-
 
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_Services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -11,27 +12,31 @@ import { AccountService } from '../_Services/account.service';
 })
 export class RegisterComponent {
   private accountService = inject(AccountService);
+  private toastr = inject(ToastrService);
   cancelRegister = output<boolean>();
   model: any = {};
 
   register() {
-    this.accountService.register(this.model).subscribe({
-      next: (response) => {
-        alert('Register successfully');
-        console.log(response);
-        this.cancel();
-      },
-      error: (err) => {
-        console.log(err.error)
-        alert('Some thing went wrong! \n' + err.error);
-      },
-      complete: this.cancel,
-    });
-    return true;
+    if (!this.model.username) {
+      this.toastr.warning('Please enter username');
+    } else if (!this.model.password) {
+      this.toastr.warning('Please enter password');
+    } else {
+      this.accountService.register(this.model).subscribe({
+        next: (response) => {
+          this.toastr.success('Register successfully');
+          this.cancel();
+        },
+        error: (err) => {
+          console.log(err.error);
+          this.toastr.error(err.error);
+        },
+        complete: this.cancel,
+      });
+    }
   }
 
   cancel() {
     this.cancelRegister.emit(false);
   }
 }
-
